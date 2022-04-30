141edd5a6421cb3dd57a021fbb4172aa82ef2dc8
xiong0827 <81082817+xiong0827@users.noreply.github.com>
2022-04-30 19:15:38 +0800

    测试

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,19 +1,14 @@
-import { Button, Tabbar, TabbarItem, Col, Row  } from 'vant';
+import { Button, Tabbar, TabbarItem, Col, Row,Grid, GridItem,Search,Tab, Tabs,Icon} from 'vant';
 import Vue from 'vue'
 Vue.use(Button);
 Vue.use(TabbarItem) 
 Vue.use(Tabbar) 
-Vue.use(Col);import { Icon } from 'vant';
-
+Vue.use(Col);
 Vue.use(Icon);
-import { Search } from 'vant';
-import { Tab, Tabs } from 'vant';
 Vue.use(Tab);
 Vue.use(Tabs);
 Vue.use(Search);
 Vue.use(Row);
-import { Grid, GridItem } from 'vant';
-
 Vue.use(Grid);
 Vue.use(GridItem);
 export default Vue
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,44 +1,20 @@
-
-
-
-
-// export default[    {
-//     path: "/list",
-//     name: "list",
-//     component: () => import("@/views/List"),
-//     meta:{
-//         footerIsShow:true
-//     }
-// },
-// {
-//     path: "/home",
-//     name: "home",
-//     component: () => import("@/views/Home"),
-//     meta:{
-//         footerIsShow:true
-//     }
-// },
-// {
-//     path: "/cart",
-//     name: "cart",
-//     component: () => import("@/views/Cart"),
-//     meta:{
-//         footerIsShow:true
-//     }
-// },
-// {
-//     path: "/my",
-//     name: "my",
-//     component: () => import("@/views/My"),
-//     meta:{
-//         footerIsShow:true
-//     }
-// },
-// {
-//     path: "*",
-//     redirect: '/home',
-//     meta: {
-//         footerIsShow: true
-//     }
-// },
-// ]
+export default [
+    {
+    path: "/list",
+    name: "list",
+    // component: () => import("@/views/List"),
+    // meta: {
+    //     footerIsShow: true
+    // }
+    
+}, 
+{
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/Login"),
+    // meta: {
+    //     footerIsShow: true
+    // }
+    
+}, 
+]
